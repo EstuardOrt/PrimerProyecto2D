45e231a47ededf37a33d59c32f1f806edf53cb83
David Mejicano <demoortiz1@gmail.com>
2026-02-03 13:57:35 -0600

    Se agrego el script HolaMundo

---
+++ Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private string mensaje;
+    void Start()
+    {
+        Saludar();
+    }
+
+    public void Saludar()
+    {
+        Debug.Log(mensaje);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
