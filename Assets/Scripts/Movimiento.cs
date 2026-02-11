0491a627b2265d2e552fb2671074970ebaa1f315
David Mejicano <demoortiz1@gmail.com>
2026-02-11 14:27:09 -0600

    Se agrego la funcion de salto

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -6,12 +6,25 @@ public class Movimiento : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private float velocidadCaminata = 4f;
+    [SerializeField] private float velInicialSalto = 24f;
+    [SerializeField] private LayerMask capaDeSalto;
     [SerializeField] private float alturaSalto = 4f;
 
     private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    public void Saltar(bool debeSaltar)
+    {
+        if(!boxCollider.IsTouchingLayers(capaDeSalto)){return; }
+        if (debeSaltar)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, velInicialSalto);
+        }
     }
 
     public void Moverse(float movimientoX)
