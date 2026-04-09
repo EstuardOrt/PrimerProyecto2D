b00b8bbc6eed896f2633aacae577ab3cdcb1336e
David Mejicano <demoortiz1@gmail.com>
2026-04-09 13:43:35 -0600

    Agregar sonido

--- Assets/Scripts/ItemFruta.cs
+++ Assets/Scripts/ItemFruta.cs
@@ -3,16 +3,34 @@ using UnityEngine;
 public class ItemFruta : MonoBehaviour
 {
     [SerializeField] private int valorFruta = 1;
+    [SerializeField] private AudioClip audioClip; 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
         Puntaje playerPuntaje = collision.GetComponent<Puntaje>();
 
         if (playerPuntaje != null)
         {
+         
             playerPuntaje.AumentarPuntos(valorFruta);
-            Destroy(gameObject); 
+
+            ReproducirSonido();
+
+            SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+            if (sprite != null) sprite.enabled = false;
+
+            Destroy(gameObject, audioClip.length);
+        }
+    }
+
+    private void ReproducirSonido()
+    {
+        if (audioClip == null) return;
+
+        AudioSource audioSource = GetComponent<AudioSource>();
+        if (audioSource != null)
+        {
+            audioSource.PlayOneShot(audioClip);
         }
     }
 }
